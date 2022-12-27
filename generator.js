c6ae4060f9fbed996296c4beda7dbb0643536ac3
Flicat.li <flicat.li@qq.com>
2022-12-27 12:58:06 +0800

    modify template

--- generator.js
+++ generator.js
@@ -1,5 +1,8 @@
 // generator.js
 module.exports = (api, options, rootOptions) => {
   // 复制template模版
-  api.render('./template');
+  api.render('./template')
+  api.render({
+    './package.json': './template/package.json'
+  })
 }
